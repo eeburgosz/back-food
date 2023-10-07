68f7466e45507f26a71b11f88a98b857c6ffab62
eeburgosz <hdst.bur@gmail.com>
2023-10-06 21:51:34 -0300

    Modificación configuración DB - 5

--- src/db.js
+++ src/db.js
@@ -22,7 +22,19 @@ const sequelize = process.env.NODE_ENV === 'production'
    //          keepAlive: true
    //       }
    //    })
-   new Sequelize(`postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/food`, { logging: false })
+   new Sequelize(`postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/food`, {
+      logging: false,
+      dialect: "postgres",
+      native: false,
+      ssl: true,
+      dialectOptions: {
+         ssl: {
+            require: true,
+            rejectUnauthorized: false
+         },
+         keepAlive: true
+      }
+   })
    :
    new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, { logging: false });
 
