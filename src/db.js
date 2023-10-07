676aa071b3d96c2cc354fa8b430033a3da261e3e
eeburgosz <hdst.bur@gmail.com>
2023-10-06 21:22:59 -0300

    Modificación configuración DB - 4

--- src/db.js
+++ src/db.js
@@ -3,25 +3,26 @@ const RecipesModel = require('./models/Recipes');
 const TypesModel = require('./models/Types');
 
 require('dotenv').config();
-const { DB_PASSWORD, DB_USER, DB_HOST, DATABASE_URL } = process.env;
+const { DB_PASSWORD, DB_USER, DB_HOST, DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD } = process.env;
 
 const sequelize = process.env.NODE_ENV === 'production'
    ?
-   new Sequelize(DATABASE_URL,
-      {
-         logging: false,
-         port: 5432,
-         dialect: "postgres",
-         native: false,
-         ssl: true,
-         dialectOptions: {
-            ssl: {
-               require: true,
-               rejectUnauthorized: false
-            },
-            keepAlive: true
-         }
-      })
+   // new Sequelize(DATABASE_URL,
+   //    {
+   //       logging: false,
+   //       port: 5432,
+   //       dialect: "postgres",
+   //       native: false,
+   //       ssl: true,
+   //       dialectOptions: {
+   //          ssl: {
+   //             require: true,
+   //             rejectUnauthorized: false
+   //          },
+   //          keepAlive: true
+   //       }
+   //    })
+   new Sequelize(`postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/food`, { logging: false })
    :
    new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, { logging: false });
 
