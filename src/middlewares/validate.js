5c686018aed6c01ec7785d288cd3fa249a7c586c
eeburgosz <hdst.bur@gmail.com>
2023-10-16 16:40:05 -0300

    Modificando validación en POST

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,6 +1,6 @@
 const validatePost = (req, res, next) => {
    const { name, img, summary, stepByStep, dishTypes, types } = req.body;
-   if (!name || !img || !summary || !stepByStep || !dishTypes || !types) {
+   if (!name || !summary || !stepByStep) {
       return res.status(400).json({ message: "Missing data" });
    }
    next();
