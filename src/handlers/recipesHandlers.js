198805ddc7b07dd5c119f3a479ef50be7c8a1012
eeburgosz <hdst.bur@gmail.com>
2023-10-17 21:03:56 -0300

    Modificación en comprobación de dato de entrada

--- src/handlers/recipesHandlers.js
+++ src/handlers/recipesHandlers.js
@@ -15,7 +15,7 @@ const getRecipesHandler = async (req, res) => {
 
 const getRecipeByIdHandler = async (req, res) => {
    const { id } = req.params;
-   if (isNaN(id)) return "ID type invalid";
+   if (isNaN(Number(id))) return "ID type invalid";
    try {
       const result = await getRecipeByIdController(id);
       result ?
