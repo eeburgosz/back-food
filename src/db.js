0c3fd87dd333bf5459442be29d11b98ea31e07b4
eeburgosz <hdst.bur@gmail.com>
2024-06-20 13:10:58 -0300

    Modificación DB

--- src/db.js
+++ src/db.js
@@ -1,38 +1,68 @@
-const { Sequelize } = require('sequelize');
-const RecipesModel = require('./models/Recipes');
-const TypesModel = require('./models/Types');
+const { Sequelize } = require("sequelize");
+const RecipesModel = require("./models/Recipes");
+const TypesModel = require("./models/Types");
 
-require('dotenv').config();
-const { DB_PASSWORD, DB_USER, DB_HOST, DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD } = process.env;
+require("dotenv").config();
+const {
+	DB_PASSWORD,
+	DB_USER,
+	DB_HOST,
+	DATABASE_URL,
+	DATABASE_USER,
+	DATABASE_PASSWORD,
+} = process.env;
 
-const sequelize = process.env.NODE_ENV === 'production'
-   ?
-   new Sequelize(`postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/food`, {
-      logging: false,
-      dialect: "postgres",
-      native: false,
-      ssl: true,
-      dialectOptions: {
-         ssl: {
-            require: true,
-            rejectUnauthorized: false
-         },
-         keepAlive: true
-      }
-   })
-   :
-   new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, { logging: false });
+// const sequelize = process.env.NODE_ENV === 'production'
+//    ?
+//    new Sequelize(`postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_URL}/food`, {
+//       logging: false,
+//       dialect: "postgres",
+//       native: false,
+//       ssl: true,
+//       dialectOptions: {
+//          ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//          },
+//          keepAlive: true
+//       }
+//    })
+//    :
+//    new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, { logging: false });
+
+const sequelize =
+	process.env.NODE_ENV === "production"
+		? new Sequelize(DATABASE_URL, {
+				logging: false,
+				dialect: "postgres",
+				native: false,
+				ssl: true,
+				dialectOptions: {
+					ssl: {
+						require: true,
+						rejectUnauthorized: false,
+					},
+					keepAlive: true,
+				},
+		  })
+		: new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+				logging: false,
+		  });
 
 RecipesModel(sequelize);
 TypesModel(sequelize);
 
 const { Recipes, Types } = sequelize.models;
 
-const RecipesTypes = sequelize.define('RecipesTypes', {}, { timestamps: false });
+const RecipesTypes = sequelize.define(
+	"RecipesTypes",
+	{},
+	{ timestamps: false }
+);
 Recipes.belongsToMany(Types, { through: RecipesTypes });
 Types.belongsToMany(Recipes, { through: RecipesTypes });
 
 module.exports = {
-   sequelize,
-   ...sequelize.models
+	sequelize,
+	...sequelize.models,
 };
