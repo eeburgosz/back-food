a80b8889523da9a291a320686e6748615ababbcf
eeburgosz <hdst.bur@gmail.com>
2024-06-21 17:57:20 -0300

    Modificación External Database URL Render

--- src/handlers/recipesHandlers.js
+++ src/handlers/recipesHandlers.js
@@ -1,44 +1,69 @@
-const { getAllRecipesController, getRecipeByNameController, getRecipeByIdController, postRecipeController } = require("../controllers/recipesControllers");
+const {
+	getAllRecipesController,
+	getRecipeByNameController,
+	getRecipeByIdController,
+	postRecipeController,
+} = require("../controllers/recipesControllers");
 
 const getRecipesHandler = async (req, res) => {
-   const { name } = req.query;
-   try {
-      const result = name ? await getRecipeByNameController(name) : await getAllRecipesController();
-      result.length === 0 ?
-         res.status(404).json({ message: "Recipes not found" })
-         :
-         res.status(200).json(result);
-   } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+	const { name } = req.query;
+	try {
+		const result = name
+			? await getRecipeByNameController(name)
+			: await getAllRecipesController();
+		console.log(result[0].stepByStep);
+		result.length === 0
+			? res.status(404).json({ message: "Recipes not found" })
+			: res.status(200).json(result);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
 };
 
 const getRecipeByIdHandler = async (req, res) => {
-   const { id } = req.params;
-   if (isNaN(Number(id))) return res.status(404).json({ message: "ID type invalid" });
-   try {
-      const result = await getRecipeByIdController(id);
-      result ?
-         res.status(200).json(result)
-         :
-         res.status(404).json({ message: "ID not found" });
-   } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+	const { id } = req.params;
+	if (isNaN(Number(id)))
+		return res.status(404).json({ message: "ID type invalid" });
+	try {
+		const result = await getRecipeByIdController(id);
+		result
+			? res.status(200).json(result)
+			: res.status(404).json({ message: "ID not found" });
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
 };
 
 const postRecipeHandler = async (req, res) => {
-   const { name, img, summary, score, healthScore, stepByStep, dishTypes, types } = req.body;
-   try {
-      const result = await postRecipeController(name, img, summary, score, healthScore, stepByStep, dishTypes, types);
-      res.status(201).json(result);
-   } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+	const {
+		name,
+		img,
+		summary,
+		score,
+		healthScore,
+		stepByStep,
+		dishTypes,
+		types,
+	} = req.body;
+	try {
+		const result = await postRecipeController(
+			name,
+			img,
+			summary,
+			score,
+			healthScore,
+			stepByStep,
+			dishTypes,
+			types
+		);
+		res.status(201).json(result);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
 };
 
 module.exports = {
-   getRecipesHandler,
-   getRecipeByIdHandler,
-   postRecipeHandler
+	getRecipesHandler,
+	getRecipeByIdHandler,
+	postRecipeHandler,
 };
--- src/utils/api.js
+++ src/utils/api.js
@@ -11,45 +11,15 @@ const stripHTML = (text) => {
 const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`;
 
 const infoApiRecipes = async () => {
-	// const stepsURL = `https://api.spoonacular.com/recipes/${d.id}/analyzedInstructions?apiKey=${API_KEY}&addRecipeInformation=true&number=5220`;
-	// const resp = (await axios.get(URL)).data.results;
-	// const data = resp.map(async (d) => ({
-	// 	id: d.id,
-	// 	name: d.title,
-	// 	img: d.image,
-	// 	diets: d.diets,
-	// 	dishTypes: d.dishTypes,
-	// 	summary: stripHTML(d.summary),
-	// 	score: d.weightWatcherSmartPoints,
-	// 	healthScore: d.healthScore,
-	// 	// stepByStep: d.analyzedInstructions[0]?.steps.map((step) => (
-	// 	//    {
-	// 	//       number: step.number,
-	// 	//       step: step.step,
-	// 	//    }
-	// 	// ))
-	// 	stepByStep: (await axios.get(stepsURL)).data.results[1].steps.map(
-	// 		(step) => ({
-	// 			number: step.number,
-	// 			step: step.step,
-	// 		})
-	// 	),
-	// }));
-	// console.log(data);
-
-	// return data;
 	const resp = (await axios.get(URL)).data.results;
-
 	const dataPromises = resp.map(async (d) => {
 		const stepsURL = `https://api.spoonacular.com/recipes/${d.id}/analyzedInstructions?apiKey=${API_KEY}&addRecipeInformation=true`;
-
 		const stepsResp = await axios.get(stepsURL);
 		const stepsData =
 			stepsResp.data[0]?.steps.map((step) => ({
 				number: step.number,
 				step: step.step,
-			})) || []; // Asegúrate de manejar el caso en que no haya pasos
-
+			})) || [];
 		return {
 			id: d.id,
 			name: d.title,
@@ -62,9 +32,7 @@ const infoApiRecipes = async () => {
 			stepByStep: stepsData,
 		};
 	});
-
 	const data = await Promise.all(dataPromises);
-
 	return data;
 };
 
